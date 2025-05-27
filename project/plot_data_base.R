f71ec9b5cebc77879321cda51d9895dafb2ab7be
TatianaIA <tatiakovleva@yahoo.fr>
2025-05-27 03:29:23 +0300

    Update plot_data_base.R

--- project/plot_data_base.R
+++ project/plot_data_base.R
@@ -1,1 +1,7 @@
+data <- read.csv("sadata.csv")
 
+pdf("plot.pdf")
+plot(data$date, data$SP.DYN.LE00.IN, type = "p", col = "blue",
+     xlab = "Year", ylab = "Life Expectancy",
+     main = "South Asia - Life Expectancy (sample)")
+dev.off()
